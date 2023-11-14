3827b53c9f7cae1cc22a113f7c731d8fd8426aa2
hsanch13 <hayleysanchez13@gmail.com>
2023-11-13 17:42:43 -0700

    made arrow function using .find

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+  function superbowlWin(record){
+  const winningRecord = record.find(obj => obj.result === "W");
+    if (winningRecord){
+        return winningRecord.year
+    }
+}
